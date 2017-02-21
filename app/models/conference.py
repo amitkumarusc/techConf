a5c7ab5a797b53d8628a45bb2485b89b3a2e9c00
Amit Kumar <amit@mavenhive.in>
2017-02-21 17:50:15 +0530

    fixed indentation

--- app/models/conference.py
+++ app/models/conference.py
@@ -4,54 +4,51 @@ from difflib import SequenceMatcher
 
 db = MongoAlchemy()
 
+
 class Conference(db.Document):
-	query_class = BaseQuery
-	name = db.StringField()
-	start_date = db.DateTimeField()
-	end_date = db.DateTimeField()
-	location = db.StringField()
-	desc = db.StringField()
-	url = db.StringField()
+    query_class = BaseQuery
+    name = db.StringField()
+    start_date = db.DateTimeField()
+    end_date = db.DateTimeField()
+    location = db.StringField()
+    desc = db.StringField()
+    url = db.StringField()
 
-	def __str__(self):
-		return '%s at %s'%(self.name, self.location)
+    def __str__(self):
+        return '%s at %s' % (self.name, self.location)
 
-	@staticmethod
-	def add_months(sourcedate, months):
-		month = sourcedate.month - 1 + months
-		year = int(sourcedate.year + month / 12 )
-		month = month % 12 + 1
-		day = min(sourcedate.day,calendar.monthrange(year,month)[1])
-		return datetime.date(year,month,day)
+    @staticmethod
+    def add_months(sourcedate, months):
+        month = sourcedate.month - 1 + months
+        year = int(sourcedate.year + month / 12)
+        month = month % 12 + 1
+        day = min(sourcedate.day, calendar.monthrange(year, month)[1])
+        return datetime.date(year, month, day)
 
-	@staticmethod
-	def fetch_all_conferences():
-		return Conference.query.all()
+    @staticmethod
+    def fetch_all_conferences():
+        return Conference.query.all()
 
-	@staticmethod
-	def fetch_upcoming_conferences(start_date):
-		all_conferences = Conference.fetch_all_conferences()
-		conferences = []
-		for conference in all_conferences:
-			if conference.start_date.date() > start_date and conference.start_date.date() < Conference.add_months(start_date, 1):
-				conferences.append(conference)
-		return conferences
+    @staticmethod
+    def fetch_upcoming_conferences(start_date):
+        all_conferences = Conference.fetch_all_conferences()
+        conferences = []
+        for conference in all_conferences:
+            if conference.start_date.date() > start_date and conference.start_date.date() < Conference.add_months(start_date, 4):
+                conferences.append(conference)
+        return conferences
 
-	@staticmethod
-	def similar(a, b):
-		return SequenceMatcher(None, a, b).ratio()
+    @staticmethod
+    def similar(a, b):
+        return SequenceMatcher(None, a, b).ratio()
 
-	@staticmethod
-	def fetch_from_location(location):
-		all_conferences = Conference.fetch_all_conferences()
-		conferences = []
-		for conference in all_conferences:
-			ratio = Conference.similar(location, conference.location.lower())
-			if ratio > 0.5:
-				conferences.append(conference)
+    @staticmethod
+    def fetch_from_location(location):
+        all_conferences = Conference.fetch_all_conferences()
+        conferences = []
+        for conference in all_conferences:
+            ratio = Conference.similar(location, conference.location.lower())
+            if ratio > 0.5:
+                conferences.append(conference)
 
-		return conferences
-
-
-
-
+        return conferences
--- app/utils/dbupdater.py
+++ app/utils/dbupdater.py
@@ -4,39 +4,35 @@ from ..models.conference import Conference
 from notifier import send_to_all_channels, format_conference_data
 import psycopg2
 
+
 def parse_date(raw_date):
-	year, month, day = map(int, raw_date.split('-'))
-	return datetime(year=year, day=day, month=month)
+    year, month, day = map(int, raw_date.split('-'))
+    return datetime(year=year, day=day, month=month)
+
 
 def fetch_conferences():
-	print "fetching json"
-	url = 'https://talkfunnel.com/json'
-	resp = requests.get(url)
-	data = resp.json()
+    print "fetching json"
+    url = 'https://talkfunnel.com/json'
+    resp = requests.get(url)
+    data = resp.json()
 
-	for conference in data['spaces']:
-		title = conference['title'].strip()
-		url = conference['url'].strip()
-		start_date = parse_date(conference['start'])
-		try:
-			end_date = parse_date(conference['end'])
-		except:
-			end_date = start_date
-		try:
-			location = str(conference['datelocation']).split(',', 1)[1].strip()
-		except:
-			location = conference['datelocation']
-		count = Conference.query.filter(Conference.name==title).count()
-		if count == 0:
-			print "Document not present. Inserting"
-			mConf = Conference(name=title, start_date=start_date, end_date=end_date, url=url, location=location, desc="")
-			mConf.save()
-			data = format_conference_data([mConf])
-			send_to_all_channels(data)
-
-
-
-
-
-
-
+    for conference in data['spaces']:
+        title = conference['title'].strip()
+        url = conference['url'].strip()
+        start_date = parse_date(conference['start'])
+        try:
+            end_date = parse_date(conference['end'])
+        except:
+            end_date = start_date
+        try:
+            location = str(conference['datelocation']).split(',', 1)[1].strip()
+        except:
+            location = conference['datelocation']
+        count = Conference.query.filter(Conference.name == title).count()
+        if count == 0:
+            print "Document not present. Inserting"
+            mConf = Conference(name=title, start_date=start_date, end_date=end_date, url=url, location=location,
+                               desc="")
+            mConf.save()
+            data = format_conference_data([mConf])
+            send_to_all_channels(data)
