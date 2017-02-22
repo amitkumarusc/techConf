b2b9275c931b0a0809c8b149f59deaa49dcaf895
Amit Kumar <amit@mavenhive.in>
2017-02-22 16:33:35 +0530

    default port from ENV variable

--- run.py
+++ run.py
@@ -1,2 +1,3 @@
 from app import app
-app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port, debug=True, use_reloader=False)
